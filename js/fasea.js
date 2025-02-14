a989c287e893f9726086425904b30303ebf78b64
pmartine082 <pmartinez082@ikasle.ehu.eus>
2025-02-14 17:17:27 +0100

    first commit

---
+++ js/fasea.js
@@ -0,0 +1,58 @@
+import {API_URL} from './konstanteak.js'
+import * as konstanteak from "./konstanteak.js";
+
+
+
+
+//FASEAREN EZAUGARRIAK LORTU
+export const getFasearenEzaugarriak = async () => {
+    
+    const fasea = await getFaseAktiboa();
+    //console.log(fasea);
+    const idFasea = fasea.idFasea; 
+    try {
+        const response = await fetch(`${API_URL}/fasea/${idFasea}/ezaugarriak`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            
+        });
+        if (response.ok) {
+            const data = await response.json();
+            const ezaugarriak = [];
+            data.forEach(ezaugarri => {
+                ezaugarriak.push(new konstanteak.Ezaugarria(ezaugarri.idEzaugarria, ezaugarri.izena, ezaugarri.puntuakMax, ezaugarri.puntuakMin));
+            });
+            return ezaugarriak;
+        }
+    } catch (err) {
+        //console.log(err);
+    }
+};
+
+export const getFaseAktiboa = async () => {
+    
+    try {
+        const response = await fetch(`${API_URL}/fasea/lortu/aktiboa`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        if (response.ok) {
+            const data = await response.json();
+            return new konstanteak.Fasea(data[0].idFasea, data[0].idTxapelketa, data[0].izena, data[0].egoera, data[0].hasiera, data[0].amaiera, data[0].irizpidea);
+        }
+        else{
+            return [];
+        }
+    } catch (err) {
+        ////console.log(err);
+        return [];
+    }
+};
+
+
+
+
---
+++ sw.js
@@ -0,0 +1,104 @@
+const VERSION = "2";
+const CACHE_NAME = `epaile-bertsio-${VERSION}`;
+const APP_STATIC_RESOURCES = [
+  "/epaile/icons/putxera.jpg",
+  "/epaile/icons/putxera.png",
+  "/epaile/css/style.css",
+  "/epaile/index.html",
+  "/epaile/index.html?",
+  "/epaile/html/epaitu.html",
+  "/epaile/html/epaitu.html?",
+  "/epaile/js/epaile.js",
+  "/epaile/js/app.js",
+  "/epaile/js/ebaluazioa.js",
+  "/epaile/js/epaimahaikidea.js",
+  "/epaile/js/fasea.js",
+  "/epaile/js/konstanteak.js",
+  "/epaile/js/taldea.js",
+  "/epaile/js/txapelketa.js",
+  "/epaile/js/user.js",
+  "/epaile/pwa/manifest.json",
+  "/epaile/sw.js",
+  "/epaile/pics/atzera.svg",
+  "/epaile/pics/berria.svg",
+  "/epaile/pics/birkargatu.svg",
+  "/epaile/pics/chef.svg",
+  "/epaile/pics/debekatuta.svg",
+  "/epaile/pics/epaitu.svg",
+  "/epaile/pics/ezarpenak.svg",
+  "/epaile/pics/historia.svg",
+  "/epaile/pics/itxaron.svg",
+  "/epaile/pics/mahaia.svg",
+  "/epaile/pics/menu.svg",
+  "/epaile/pics/podium.svg",
+  "icons/putxera.jpg",
+  "icons/putxera.png",
+ 
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return Promise.all(
+        APP_STATIC_RESOURCES.map((resource) =>
+          cache.add(resource).catch((error) => console.error(`❌ Error cacheando ${resource}:`, error))
+        )
+      );
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => {
+      return Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            console.log(`Deleting old cache: ${name}`);
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("Interceptando petición:", event.request.url);
+  if (!event.request.url.startsWith(self.location.origin)) {
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        console.log("Sirviendo desde caché:", event.request.url);
+        return cachedResponse;
+      }
+      console.log("No está en caché, intentamos obtener de la red:", event.request.url);
+      return fetch(event.request)
+        .then((networkResponse) => {
+          if (!networkResponse || networkResponse.status !== 200 || event.request.method !== "GET") {
+            return networkResponse;
+          }
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        })
+        .catch(() => {
+          console.error("⚠️ Sin conexión y recurso no cacheado:", event.request.url);
+          if (event.request.mode === "navigate") {
+            return caches.match("/index.html");
+          }
+          return caches.match(event.request).then((fallbackResponse) => {
+            if (fallbackResponse) {
+              return fallbackResponse;
+            }
+            return new Response("⚠️ Offline: El recurso no está en caché.", { status: 404 });
+          });
+        });
+    })
+  );
+});
